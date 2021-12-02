fa7c789e11c626e88a50f13ab9d07fa871df887f
unknown <bojanabakic96@gmail.com>
2021-12-02 17:02:36 +0200

    commit 3.21

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ app.use(unknownEndpoint)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

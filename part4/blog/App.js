e91990630220427cfc0e26f2e47f05bca59c2131
bojanabakic <bojanabakic96@gmail.com>
2022-01-09 18:21:35 +0200

    Add files via upload

--- part4/blog/App.js
+++ part4/blog/App.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/user')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -20,9 +22,16 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use(middleware.requestLogger)
+
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use("/api/blogs", middleware.userExtractor, blogRouter)
+
 module.exports = app

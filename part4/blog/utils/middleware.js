41cb344d746b5dd94fb05ea1453df6dd3f1ffbca
bojanabakic <bojanabakic96@gmail.com>
2022-01-09 18:23:19 +0200

    Add files via upload

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require("jsonwebtoken")
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -13,19 +15,54 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({
+      error: error.message
+    })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
+  logger.error(error.message)
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request['token'] = authorization.substring(7)
+  }
+  next()
+}
+ 
+const userExtractor = async (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  const decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+    request.user = user
+  }else{
+    request.user = null
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
+  userExtractor
 }

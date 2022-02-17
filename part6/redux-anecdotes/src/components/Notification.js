6a389765a0089b9846058552f6fb86c09fcaa621
Bojana Bakić <bojanabakic96@gmail.com>
2022-02-17 21:21:52 +0200

    Update Notification.js

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -7,40 +7,8 @@ const Notification = (props) => {
     padding: 10,
     borderWidth: 1
   }
-  if(props.notification){
-    return (
-      <div style={style}>
-        {props.notification}
-      </div>
-    )
-  }else{
-    return null
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    notification: state.notification,
-  }
-}
-
-export default connect(mapStateToProps)(Notification)
-
-
-
-/*import React from 'react'
-import { connect } from 'react-redux'
-
-const Notification = (props) => {
-  const notification = props.notification
-
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1
-  }
-
-  if (!notification) {
+  
+  if (!props.notification) {
     return null
   } else {
     return (
@@ -53,8 +21,8 @@ const Notification = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    notification: state.notifications,
+    notification: state.notification,
   }
 }
 
-export default connect(mapStateToProps)(Notification)*/
+export default connect(mapStateToProps)(Notification)
